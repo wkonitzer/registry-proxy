7215d6eda8afcd7c706bc2108044c42153934eda
William Konitzer <william@konitzer.com>
2024-03-21 16:13:44 +0000

    Add support for eventlet

--- start.sh
+++ start.sh
@@ -5,8 +5,10 @@ if [ "$MODE" = "https" ]; then
     # Start Gunicorn with SSL configuration
     exec gunicorn --certfile /etc/ssl/certs/server.crt \
                   --keyfile /etc/ssl/certs/server.key \
-                  --log-level $LOGGING_LEVEL -w 1 -b 0.0.0.0:8443 container_proxy:app
+		  --worker-class eventlet \
+                  --log-level $LOGGING_LEVEL \
+		  -w 3 -b 0.0.0.0:8443 container_proxy:app
 else
     # Start Gunicorn without SSL configuration
-    exec gunicorn --log-level $LOGGING_LEVEL -w 1 -b 0.0.0.0:8080 container_proxy:app
+    exec gunicorn --worker-class eventlet --log-level $LOGGING_LEVEL -w 3 -b 0.0.0.0:8080 container_proxy:app
 fi
